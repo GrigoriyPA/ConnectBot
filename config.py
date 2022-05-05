641fc5c51721413685af220f361a883951f6fb02
GrigoriyPA <79596613+GrigoriyPA@users.noreply.github.com>
2022-05-05 23:04:08 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+VK_TOKEN = ""
+VK_GROUP_ID = 0
+
+DISCORD_TOKEN = ""
+
+TELEGRAM_TOKEN = ""
+
+TOKEN_LENGTH = 10
+
+GRAPH_STORAGE_NAME = "data/graph.txt"
+ERROR_LOG_NAME = "data/error_log.txt"
+USERS_INFORMATION_DB_NAME = "data/users_inf.db"
